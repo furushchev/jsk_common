434bd5b160529b38bbf42cbdbb2555d2bb1f46e6
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-12-23 13:44:09 +0900

    jsk_topic_tools: minor update

--- jsk_topic_tools/src/synchronized_throttle_nodelet.cpp
+++ jsk_topic_tools/src/synchronized_throttle_nodelet.cpp
@@ -97,7 +97,7 @@ void SynchronizedThrottle::onInit()
   {
     check_timer_ = pnh_->createWallTimer(
         ros::WallDuration(5), &SynchronizedThrottle::checkAdvertisedTimerCallback,
-        this, false);
+        this, /* oneshot= */false);
   }
 }
 
@@ -142,7 +142,7 @@ void SynchronizedThrottle::checkAdvertisedTimerCallback(const ros::WallTimerEven
   {
     if (!pub_[i])
     {
-      ROS_WARN_STREAM(input_topics_[i] << " is not yet published");
+      NODELET_WARN_STREAM(input_topics_[i] << " is not yet published");
     }
   }
   if (advertised_)
